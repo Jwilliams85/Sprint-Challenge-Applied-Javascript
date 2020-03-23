b9da4c5d7c038fe30c80ff3eb891d27d39a73d1b
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-03-22 23:48:32 -0400

    carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,41 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel() {
+
+  //Create Element
+
+  const div1 = document.createElement ('div')
+  const div2 = document.createElement ('div')
+  const img1 = document.createElement ('img')
+  img1.src = "./assets/carousel/mountains.jpeg" 
+  const img2 = document.createElement ('img')
+  img2.src = "./assets/carousel/computer.jpeg"
+  const img3 = document.createElement ('img')
+  img3.src = "./assets/carousel/trees.jpeg"
+  const img4 = document.createElement ('img')
+  img3.src = "./assets/carousel/turntable.jpeg" 
+  const div3 = document.createElement ('div')
+
+ //Create HTML Structure
+
+div1.appendChild(div2)
+div1.appendChild(div3)
+div1.appendChild(img1)
+div1.appendChild(img2)
+div1.appendChild(img3)
+div1.appendChild(img4)
+
+//Create Classes
+
+div1.classList.add('carousel')
+div2.classList.add('left-button')
+div3.classList.add('right-button')
+
+
+return div1
+
+}
+
+const container = document.querySelector('.carousel-container').appendChild(createCarousel())
